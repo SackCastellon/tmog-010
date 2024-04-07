1fc5b5edb90f15a32dacdeeadac32fc57d34d63a
Juan José González Abril <juanjo@gonzabril.es>
2024-04-07 20:32:56 +0200

    Implement solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,61 @@
 package org.tfoc;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public List<List<String>> solveNQueens(int n) {
+  private static final char QUEEN = 'Q';
 
-        return null;
+  public List<List<String>> solveNQueens(int n) {
+    if (n < 1) throw new IllegalArgumentException("number of queens must be greater than 0");
+    if (n == 2 || n == 3) return List.of();
+
+    return solve(new int[0], n).map(it -> toString(it, n)).toList();
+  }
+
+  private static Stream<int[]> solve(int[] rows, int n) {
+    if (rows.length == n) return Stream.of(rows);
+    return IntStream.range(0, n)
+        .parallel()
+        .filter(col -> isSafe(rows, col))
+        .boxed()
+        .flatMap(col -> solve(addColumn(rows, col), n));
+  }
+
+  private static boolean isSafe(int[] rows, int newCol) {
+    var rowCount = rows.length;
+    for (int row = 0; row < rowCount; row++) {
+      var col = rows[row];
+
+      var left = newCol - (rowCount - row);
+      var right = newCol + (rowCount - row);
+
+      if (col == left || col == newCol || col == right) {
+        return false;
+      }
     }
+
+    return true;
+  }
+
+  private static int[] addColumn(int[] ints, int col) {
+    var copy = Arrays.copyOf(ints, ints.length + 1);
+    copy[ints.length] = col;
+    return copy;
+  }
+
+  private static List<String> toString(int[] rows, int n) {
+    return Arrays.stream(rows).mapToObj(col -> {
+      var builder = new StringBuilder(".".repeat(n));
+      builder.setCharAt(col, QUEEN);
+      return builder.toString();
+    }).toList();
+  }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,122 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @Slf4j
 class SolutionTest {
+  private Solution solution;
 
-    @Test
-    void testSolution() {
+  static Stream<Arguments> queensN() {
+    return Stream.of(
+        arguments(1, 1),
+        arguments(2, 0),
+        arguments(3, 0),
+        arguments(4, 2),
+        arguments(5, 10),
+        arguments(6, 4),
+        arguments(7, 40),
+        arguments(8, 92),
+        arguments(9, 352),
+        arguments(10, 724),
+        arguments(11, 2680),
+        arguments(12, 14200),
+        arguments(13, 73712),
+        arguments(14, 365596),
+        arguments(15, 2279184),
+        arguments(16, 14772512)
+    );
+  }
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
-    }
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
 
+  @Test
+  void queens1() {
+    var result = solution.solveNQueens(1);
+
+    assertThat(result).containsExactlyInAnyOrder(
+        List.of("Q")
+    );
+  }
+
+  @Test
+  void queens4() {
+    var result = solution.solveNQueens(4);
+
+    assertThat(result).hasSize(2).containsExactlyInAnyOrder(
+        List.of(
+            ".Q..",
+            "...Q",
+            "Q...",
+            "..Q."
+        ),
+        List.of(
+            "..Q.",
+            "Q...",
+            "...Q",
+            ".Q.."
+        )
+    );
+  }
+
+  @Test
+  void queens6() {
+    var result = solution.solveNQueens(6);
+
+    assertThat(result).hasSize(4).containsExactlyInAnyOrder(
+        List.of(
+            "..Q...",
+            ".....Q",
+            ".Q....",
+            "....Q.",
+            "Q.....",
+            "...Q.."
+        ),
+        List.of(
+            "...Q..",
+            "Q.....",
+            "....Q.",
+            ".Q....",
+            ".....Q",
+            "..Q..."
+        ),
+        List.of(
+            ".Q....",
+            "...Q..",
+            ".....Q",
+            "Q.....",
+            "..Q...",
+            "....Q."
+        ),
+        List.of(
+            "....Q.",
+            "..Q...",
+            "Q.....",
+            ".....Q",
+            "...Q..",
+            ".Q...."
+        )
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void queensN(int n, int expected) {
+    var result = solution.solveNQueens(n);
+
+    assertThat(result).hasSize(expected);
+  }
 }
